78a4b5ed717e47e4845f09111eac72d2d071157c
Eloi Strée <eloistree@gmail.com>
2024-09-06 23:03:37 +0200

    Save: 09/06/2024 11:03 PM

---
+++ Editor/ChangeColorMonoEditor.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ChangeColorMono))]
+public class ChangeColorMonoEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ChangeColorMono myScript = (ChangeColorMono)target;
+        if (GUILayout.Button("Change Color"))
+        {
+            myScript.SwitchColor();
+        }
+    }
+}
---
+++ Runtime/ChangeColorMono.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorMono : MonoBehaviour
+{
+    public void SwitchColor()
+    {
+        gameObject.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+    }
+
+    public void Start()
+    {
+        SwitchColor();
+    }
+}
